2869799c2325d520637e8967b827cc643fb647d1
killerwhiteshark <killerwhiteshark@users.noreply.github.com>
2020-03-05 15:53:47 -0800

    Updates

---
+++ precache-manifest.d53151fa3478c3dd928b3d3fe1c1fc3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b193da586139438b6094ad1ab01091b",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "c6d599bbd758bdec8cbd",
+    "url": "/project-4-assessment/static/css/main.dc899340.chunk.css"
+  },
+  {
+    "revision": "30e6c1223448795c9b45",
+    "url": "/project-4-assessment/static/js/2.fe9083d4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/project-4-assessment/static/js/2.fe9083d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6d599bbd758bdec8cbd",
+    "url": "/project-4-assessment/static/js/main.fd4cc6d7.chunk.js"
+  },
+  {
+    "revision": "710040f4d9954f54e398",
+    "url": "/project-4-assessment/static/js/runtime-main.c1c16f88.js"
+  }
+]);
